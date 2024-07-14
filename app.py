0b33271374182dd2f88d69847bc99eafe58eb784
Danusha <danush99online@gmail.com>
2024-07-14 08:59:30 +0530

    remake the website

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
 from model import preprocess_and_predict
 
 app = Flask(__name__)
@@ -12,6 +12,10 @@ def translate():
     sinhala_text = request.form['sinhala_text']
     prediction = preprocess_and_predict(sinhala_text)  
     return jsonify({'prediction': prediction})  
+
+@app.route('/output/<path:filename>')
+def serve_output_file(filename):
+    return send_from_directory('output', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
